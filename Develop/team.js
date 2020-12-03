103d9fc91acff972852ac35a22a56c084b19e92e
Wagon Wheel <70062069+njleoni@users.noreply.github.com>
2020-12-02 21:21:02 -0700

    test inquire prompts

--- Develop/team.js
+++ Develop/team.js
@@ -1,81 +1,116 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 
-function start() {
+const manager = ["Manager"];
+const engineer = ["Engineer"]
+const intern = ["Intern"]
+
+function managerRole() {
 
     inquirer
         .prompt([
             {
                 type: 'input',
                 name: 'name',
-                message: 'What is your name?',
+                message: 'Manager\'s name?',
             },    
             {
                 type: 'input',
                 name: 'idInfo',
-                message: 'What is your employee ID?',
+                message: 'Manager\'s employee ID?',
             },
             {
                 type: 'input',
                 name: 'email',
-                message: 'What is your email address?',
+                message: 'Manager\'s email address?',
+            },
+            {
+                type: 'input',
+                name: 'office',
+                message: 'What is your office number?',
             },
             {
                 type: 'list',
-                name: 'role',
-                message: 'What is your role?',
-                choices: ['Manager', 'Engineer', 'Intern'],
+                name: 'morePeople',
+                message: 'Are there more team members?',
+                choices: ['Yes', 'No'],
             },                        
         ])
     
         .then((data) => {
-            role(data);
-            writeInfo(data);
+            manager.push(data.name);
+            manager.push(data.idInfo);
+            manager.push(data.email);
+            manager.push(data.office);
 
+            if (data.morePeople === "Yes") {
+                employeeRole();
+            } else {
+                writeInfo(data);
+            }
         })  
 }
 
-function role(data) {
-
-  const path = `${data.role}`;
-
-  switch (path) {
-    case 'Manager':
-      return managerRole();
-
-    case 'Engineer':
-      return engineerRole();
-
-    case 'Intern':
-      return internRole();
-};
+function employeeRole() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'role',
+                message: 'What is your role?',
+                choices: ['Engineer', 'Intern'],
+            },            
+        ])
+        .then(data => {
+            if (data.role === "Engineer") {
+                engineerRole(data);
+            } else {
+                internRole(data);;
+            }
+        })  
 }
 
-function managerRole() {
+function engineerRole(data) {
     inquirer
         .prompt([
             {
                 type: 'input',
-                name: 'phone',
-                message: 'What is your office number?',
-            },            
-        ])
-        .then((data) => {
-            writeInfo(data);
-        })  
-}
-
-function engineerRole() {
-    inquirer
-        .prompt([
+                name: 'name',
+                message: 'Engineer name?',
+            },    
+            {
+                type: 'input',
+                name: 'idInfo',
+                message: 'Engineer\'s employee ID?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Engineer\'s email address?',
+            },
             {
                 type: 'input',
                 name: 'gitUser',
                 message: 'What is your GitHub user name?',
             },            
+            {
+                type: 'list',
+                name: 'morePeople',
+                message: 'Are there more team members?',
+                choices: ['Yes', 'No'],
+            },
         ])
         .then((data) => {
-            writeInfo(data);
+            engineer.push(data.name);
+            engineer.push(data.idInfo);
+            engineer.push(data.email);
+            engineer.push(data.github);
+
+            if (data.morePeople === "Yes") {
+                employeeRole();
+            } else {
+                writeInfo(data);
+            }
         })  
 }
 
@@ -84,30 +119,60 @@ function internRole() {
         .prompt([
             {
                 type: 'input',
+                name: 'name',
+                message: 'Intern\'s name?',
+            },    
+            {
+                type: 'input',
+                name: 'idInfo',
+                message: 'Intern\'s employee ID?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Intern\'s email address?',
+            },            
+            {
+                type: 'input',
                 name: 'school',
                 message: 'What school do you go to?',
             },            
+            {
+                type: 'list',
+                name: 'morePeople',
+                message: 'Are there more team members?',
+                choices: ['Yes', 'No'],
+            },
         ])
         .then((data) => {
-            writeInfo(data);
+            intern.push(data.name);
+            intern.push(data.idInfo);
+            intern.push(data.email);
+            intern.push(data.school);
+                        
+            if (data.morePeople === "Yes") {
+                employeeRole();
+            } else {
+                writeInfo(data);
+            }
         })  
 }
 
-function writeInfo(data) {
-    const redmeFile = `
-# ${data.name}
-# ${data.idInfo}
-# ${data.email}
-# ${data.role}
-# ${data.phone}
-# ${data.gitUser}
-# ${data.school}
-`;
 
-    fs.writeFile('README.md', redmeFile, (err) =>
-        err ? console.log(err) : console.log("success!")
+function writeInfo(data) {
+    console.log(manager);
+    console.log(engineer);
+    console.log(intern);
+    const redmeFile = `
+    # Test
+    
+    `;
+    
+    fs.writeFile('./output/Team.js', redmeFile, (err) =>
+    err ? console.log(err) : console.log("success!")
     )
 }
 
-start();
+
+managerRole();
 
